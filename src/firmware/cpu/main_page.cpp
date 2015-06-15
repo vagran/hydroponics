10a76dd527622c1fcdf832e049fe4e99ccd996e2
Artyom Lebedev <artyom.lebedev@gmail.com>
2015-06-15 21:22:32 +0300

    Water level drawing.

--- src/firmware/cpu/main_page.cpp
+++ src/firmware/cpu/main_page.cpp
@@ -23,6 +23,10 @@ MainPage::MainPage()
     closeRequested = false;
     pumpActive = false;
     drainActive = false;
+
+    //XXX
+    watLevelBottom = MAX_WATER_LEVEL;
+    watLevelTop = 0;
 }
 
 void
@@ -39,7 +43,23 @@ MainPage::OnRotEncClick(bool dir __UNUSED/*XXX*/)
     AtomicSection as;
     pumpActive = !pumpActive;
     drainActive = !drainActive;
-    Draw(DrawMask::M_PUMP | DrawMask::M_DRAIN);
+    if (dir) {
+        if (watLevelBottom < MAX_WATER_LEVEL) {
+            watLevelBottom++;
+        }
+        if (watLevelTop > 0) {
+            watLevelTop--;
+        }
+    } else {
+        if (watLevelBottom > 0) {
+            watLevelBottom--;
+        }
+        if (watLevelTop < MAX_WATER_LEVEL) {
+            watLevelTop++;
+        }
+    }
+    Draw(DrawMask::M_PUMP | DrawMask::M_DRAIN | DrawMask::M_BOTTOM_WATER |
+         DrawMask::M_TOP_WATER);
 }
 
 void
@@ -139,7 +159,7 @@ MainPage::IssueDrawRequest()
 
         case DrawState::DRAIN:
             if (!(drawMask & DrawMask::M_DRAIN)) {
-                drawState = DrawState::DONE;//XXX
+                drawState = DrawState::TOP_WATER;
                 break;
             }
             drawMask &= ~DrawMask::M_DRAIN;
@@ -150,6 +170,28 @@ MainPage::IssueDrawRequest()
                 bitmapWriter.Clear(POT_COL + 17, POT_PAGE + 1,
                                    &bitmaps.SyphonDrain, false, _DrawHandler);
             }
+            return;
+
+        case DrawState::TOP_WATER:
+            if (!(drawMask & DrawMask::M_TOP_WATER)) {
+                drawState = DrawState::BOTTOM_WATER;
+                break;
+            }
+            drawMask &= ~DrawMask::M_TOP_WATER;
+            display.Output(Display::Viewport{POT_COL + 1, POT_COL + 15,
+                                             POT_PAGE, POT_PAGE + 2},
+                           _DisplayOutputHandler);
+            return;
+
+        case DrawState::BOTTOM_WATER:
+            if (!(drawMask & DrawMask::M_BOTTOM_WATER)) {
+                drawState = DrawState::DONE;
+                break;
+            }
+            drawMask &= ~DrawMask::M_BOTTOM_WATER;
+            display.Output(Display::Viewport{POT_COL + 1, POT_COL + 22,
+                                             POT_PAGE + 3, POT_PAGE + 5},
+                           _DisplayOutputHandler);
             return;
 
         default:
@@ -182,10 +224,78 @@ MainPage::_DrawHandler()
     return static_cast<MainPage *>(app.CurPage())->DrawHandler();
 }
 
-bool
-MainPage::DisplayOutputHandler(u8 , u8 , u8 *)
+/** Get byte which has numBits most significant bits set. */
+static inline u8
+GetFillBits(u8 numBits)
 {
-    //XXX
+    u8 result = 0;
+    while (numBits) {
+        result = (result >> 1) | 0x80;
+        numBits--;
+    }
+    return result;
+}
+
+bool
+MainPage::DisplayOutputHandler(u8 column, u8 page, u8 *data)
+{
+    u8 _data = 0;
+    if (drawState == DrawState::TOP_WATER) {
+
+        if (page == POT_PAGE) {
+            _data = 0b00000001;
+            if (watLevelTop > 15) {
+                _data |= GetFillBits(watLevelTop - 15);
+            }
+        } else if (page == POT_PAGE + 1) {
+            if (watLevelTop >= 15) {
+                _data = 0xff;
+            } else if (watLevelTop > 7) {
+                _data = GetFillBits(watLevelTop - 7);
+            } else {
+                _data = 0;
+            }
+        } else {
+            if (watLevelTop >= 7) {
+                _data = 0xff;
+            } else {
+                _data = 0x80 | (GetFillBits(watLevelTop) >> 1);
+            }
+        }
+
+        if (column == POT_COL + 15 && page == POT_PAGE + 2) {
+            DrawHandler();
+        }
+
+    } else if (drawState == DrawState::BOTTOM_WATER) {
+
+        if (page == POT_PAGE + 3) {
+            if (watLevelBottom > 15) {
+                _data = GetFillBits(watLevelBottom - 15);
+            } else {
+                _data = 0;
+            }
+        } else if (page == POT_PAGE + 4) {
+            if (watLevelBottom >= 15) {
+                _data = 0xff;
+            } else if (watLevelBottom > 7) {
+                _data = GetFillBits(watLevelBottom - 7);
+            } else {
+                _data = 0;
+            }
+        } else {
+            if (watLevelBottom >= 7) {
+                _data = 0xff;
+            } else {
+                _data = 0x80 | (GetFillBits(watLevelBottom) >> 1);
+            }
+        }
+
+        if (column == POT_COL + 22 && page == POT_PAGE + 5) {
+            DrawHandler();
+        }
+    }
+    *data = _data;
     return true;
 }
 
--- src/firmware/cpu/main_page.h
+++ src/firmware/cpu/main_page.h
@@ -33,7 +33,8 @@ public:
 private:
     enum {
         POT_COL = 104,
-        POT_PAGE = 1
+        POT_PAGE = 1,
+        MAX_WATER_LEVEL = 21
     };
 
     enum DrawState {
@@ -47,15 +48,19 @@ private:
 
         PUMP,
         DRAIN,
+        TOP_WATER,
+        BOTTOM_WATER,
         DONE
     };
 
     enum DrawMask {
-        M_STATIC =      0x0001,
-        M_PUMP =        0x0002,
-        M_DRAIN =       0x0004,
+        M_STATIC =          0x0001,
+        M_PUMP =            0x0002,
+        M_DRAIN =           0x0004,
+        M_TOP_WATER =       0x0008,
+        M_BOTTOM_WATER =    0x0010,
 
-        M_ALL = M_STATIC | M_PUMP | M_DRAIN
+        M_ALL = M_STATIC | M_PUMP | M_DRAIN | M_TOP_WATER | M_BOTTOM_WATER
     };
 
     u8 drawState:5,
@@ -63,8 +68,12 @@ private:
        closeRequested:1,
        pumpActive:1,
 
+       watLevelTop:5,
        drainActive:1,
-       reserved:7;
+       reserved:2,
+
+       watLevelBottom:5,
+       reserved2:3;
 
     u16 drawMask;
 
